f2d5936ef1215cfa321d9a8dd4b1e2a7b97dbee0
hereiam123 <bdemaio1@gmail.com>
2020-11-22 16:17:43 -0500

    Applying graphql server to express app

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,25 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
-  app.get("/", async (_, res) => {
+  const apolloSever = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  /*app.get("/", async (_, res) => {
     const posts = await orm.em.find(Post, {});
     res.send(posts);
-  });
+  });*/
+  apolloSever.applyMiddleware({ app });
   app.listen(4000, () => {
     console.log("Server up on port 4000");
   });
