00175a7d45a4fb2bc6102d515df5125754aa8fda
hereiam123 <bdemaio1@gmail.com>
2020-11-22 16:42:29 -0500

    Adding single post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,19 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() ctx: MyContext): Promise<Post[]> {
     return ctx.em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() ctx: MyContext
+  ): Promise<Post | null> {
+    return ctx.em.findOne(Post, { id });
   }
 }
