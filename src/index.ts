2ca39a151c59bf8bdf37923a0f8697c18373ec71
hereiam123 <bdemaio1@gmail.com>
2020-11-22 16:08:57 -0500

    Setting up express

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  //const post = orm.em.create(Post, { title: "my first post" });
-  //await orm.em.persistAndFlush(post);
-  //const posts = await orm.em.find(Post, {});
-  //console.log(posts);
+  const app = express();
+  app.get("/", async (_, res) => {
+    const posts = await orm.em.find(Post, {});
+    res.send(posts);
+  });
+  app.listen(4000, () => {
+    console.log("Server up on port 4000");
+  });
 };
 
 main().catch((err) => {
