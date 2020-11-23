11a8d4d6e9835b0d3a2db4a8a2c6afce6b6d8930
hereiam123 <bdemaio1@gmail.com>
2020-11-23 14:38:36 -0500

    Adding in redis server for session storage

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,36 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
-  const apolloSever = new ApolloServer({
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+        httpOnly: true,
+        secure: __prod__,
+      },
+      secret: "qwertysstring",
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false,
@@ -20,11 +44,8 @@ const main = async () => {
       em: orm.em,
     }),
   });
-  /*app.get("/", async (_, res) => {
-    const posts = await orm.em.find(Post, {});
-    res.send(posts);
-  });*/
-  apolloSever.applyMiddleware({ app });
+
+  apolloServer.applyMiddleware({ app });
   app.listen(4000, () => {
     console.log("Server up on port 4000");
   });
