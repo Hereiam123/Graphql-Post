faf04792d0f2ee3c9f256370b181600e61723727
hereiam123 <bdemaio1@gmail.com>
2020-11-24 15:22:37 -0500

    Pushing username and password to server

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,16 +3,19 @@ import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/react";
 import Wrapper from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
+  const [registerResult, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant={"small"}>
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={(values) => {
           console.log(values);
+          register(values);
         }}
       >
         {({ values, handleChange, isSubmitting }) => (
@@ -30,7 +33,6 @@ const Register: React.FC<RegisterProps> = ({}) => {
                 type="password"
               />
             </Box>
-
             <Button
               mt={4}
               type="submit"
@@ -45,5 +47,16 @@ const Register: React.FC<RegisterProps> = ({}) => {
     </Wrapper>
   );
 };
+
+const REGISTER_MUTATION = `
+  mutation ($username: String!, $password: String!) {
+    register (options: {username: $username, password: $password}) {
+        user{
+        id
+        username
+        }
+    }
+  }
+`;
 
 export default Register;
